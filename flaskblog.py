2e8eb813177b895627cee1197d0e2d6756b83af2
kutuanonline199@gmail.com <kutuanonline199@gmail.com>
2022-05-11 07:14:04 +0700

    first commit

---
+++ flaskblog.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, url_for, request, redirect
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '58fed6d2d4e1eae1c6e0ad8bf0a80d57'
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route('/')
+def index():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
