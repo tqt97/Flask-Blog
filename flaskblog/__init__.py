e38f6e3918c5e26dad14f043fc3df0c5dd2ed32f
QuocTuanIT <kutuanonline199@gmail.com>
2022-05-13 14:14:40 +0700

    'fix'

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -18,11 +18,17 @@ mail = Mail()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
+
     app.config.from_object(Config)
+
     db.init_app(app)
+
     bcrypt.init_app(app)
+
     login_manager.init_app(app)
+
     mail.init_app(app)
+
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
